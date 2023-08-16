480519bc2901514a0c0327840f785a80dea04bc7
Usman Monir <usmanmonir026@gmail.com>
2023-08-16 08:32:18 +0500

    added simple lookup profile functionality

--- devarx_backend/post/api.py
+++ devarx_backend/post/api.py
@@ -1,26 +1,36 @@
 from rest_framework.decorators import api_view, authentication_classes
 from .models import Post
+from account.models import User
 from .forms import CreatePostForm
 from django.http import JsonResponse
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 # Create your views here.
 
 @api_view(['GET'])
-def all_posts(request):
+def allPosts(request):
     posts = Post.objects.all()
-    serialized_posts = PostSerializer(posts, many=True)
-    return JsonResponse({'posts': serialized_posts.data})
+    serializedPosts = PostSerializer(posts, many=True)
+    return JsonResponse({'posts': serializedPosts.data})
 
 
 @api_view(['POST'])
-def create_post(request):
+def createPost(request):
     data = request.data
     form = CreatePostForm(data)
     if form.is_valid():
         newPost = form.save(commit=False)
         newPost.created_by = request.user
         newPost.save()
-        serialized_post = PostSerializer(newPost)
-        return JsonResponse({'newPost': serialized_post.data})
+        serializedPosts = PostSerializer(newPost)
+        return JsonResponse({'newPost': serializedPosts.data})
     else:
         return JsonResponse({'newPost': None})
+
+
+@api_view(['GET'])
+def getUserPosts(request, id):
+    user = User.objects.get(pk=id)
+    posts = Post.objects.filter(created_by=user)
+    serializedPosts = PostSerializer(posts, many=True)
+    serializedUser = UserSerializer(user)
+    return JsonResponse({'posts': serializedPosts.data, 'user': serializedUser.data})
--- devarx_backend/post/urls.py
+++ devarx_backend/post/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import api
 
 urlpatterns = [
-    path('', api.all_posts, name='all_posts'),
-    path('create_post/', api.create_post, name='createPost')
+    path('', api.allPosts, name='allPosts'),
+    path('profile/<uuid:id>/', api.getUserPosts, name='getUserPosts'),
+    path('createPost/', api.createPost, name='createPost')
 ]
 
