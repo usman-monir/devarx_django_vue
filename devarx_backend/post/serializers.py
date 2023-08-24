3bb3a9eadaa3e17a8ac2087407d525289e9924d5
Usman Monir <usmanmonir026@gmail.com>
2023-08-24 08:08:31 +0500

    created a new component for creating post and emitting some events to update in frontend.

--- devarx_backend/post/serializers.py
+++ devarx_backend/post/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from account.serializers import UserSerializer
-from .models import Post, Comment, Trend
+from .models import Post, PostAttachment, Comment, Trend
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -9,13 +9,22 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = ('id', 'body', 'created_at_formatted', 'created_by')
 
+
+class PostAttachmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostAttachment
+        fields = ('id', 'get_image',)
+
+
 class PostSerializer(serializers.ModelSerializer):
     created_by = UserSerializer(read_only=True)
     likes = UserSerializer(many=True)
     comments = CommentSerializer(many=True)
+    attachments = PostAttachmentSerializer()
     class Meta:
         model = Post
-        fields = ('id', 'body', 'created_at_formatted', 'created_by', 'likes', 'comments',)
+        fields = ('id', 'body', 'created_at_formatted', 'created_by', 'likes', 'comments', 'attachments',)
+
 
 class TrendSerializer(serializers.ModelSerializer):
     class Meta:
